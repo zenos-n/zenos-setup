df14ce5a7da97947245873d0311d30b001eb5341
doromiert <doromiert@gmail.com>
2026-04-13 14:38:44 +0200

    ANIM ANIM ANIM

--- flake.nix
+++ flake.nix
@@ -5,43 +5,67 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
-  pname = "zenos-setup";
-  version = "0.1.0";
-  src = ./.;
+    in
+    {
+      packages.${system} = rec {
+      
+      zenos-frames = pkgs.stdenv.mkDerivation {
+        pname = "zenos-frames";
+        version = "0.1.0";
+        src = /home/doromiert/3D/dest2;
 
-  nativeBuildInputs = with pkgs; [
-    meson
-    ninja
-    pkg-config
-    gobject-introspection
-    wrapGAppsHook4
-    desktop-file-utils
-    appstream-glib
-    appstream
-    libxml2
-    glib # for glib-compile-schemas
-    python3 # needed for the build scripts
-  ];
+        installPhase = ''
+          mkdir -p $out/share/zenos/frames
+          cp frame_*.png $out/share/zenos/frames/
+        '';
+      };
+      
+      default = pkgs.stdenv.mkDerivation {
+        pname = "zenos-setup";
+        version = "0.1.0";
+        src = ./.;
 
-  buildInputs = with pkgs; [
-    gtk4
-    libadwaita
-    python3
-    python3Packages.pygobject3
-    python3Packages.babel
-  ];
+        nativeBuildInputs = with pkgs; [
+          meson
+          ninja
+          pkg-config
+          gobject-introspection
+          wrapGAppsHook4
+          desktop-file-utils
+          appstream-glib
+          appstream
+          libxml2
+          glib
+          python3
+        ];
 
-  # This ensures the final binary in /bin finds its python deps
-  postInstall = ''
-    wrapProgram $out/bin/zenos-setup \
-      --prefix PYTHONPATH : "$PYTHONPATH" \
-      --prefix GI_TYPELIB_PATH : "$GI_TYPELIB_PATH"
-  '';
-};
-};}
+        buildInputs = with pkgs; [
+          gtk4
+          libadwaita
+          python3
+          python3Packages.pygobject3
+          python3Packages.babel
+        ];
+
+        postInstall = ''
+          wrapProgram $out/bin/zenos-setup \
+            --prefix PYTHONPATH : "$PYTHONPATH" \
+            --prefix GI_TYPELIB_PATH : "$GI_TYPELIB_PATH"
+        '';
+      };
+
+      # this just wraps the `default` package, so nix doesn't rebuild the whole thing twice
+      oobe = pkgs.writeShellScriptBin "oobe" ''
+	  # point directly to your home dir, completely skipping the store copy
+	  export ZENOS_FRAMES_DIR="/home/doromiert/3D/dest2"
+	  exec ${default}/bin/zenos-setup --oobe "$@"
+      '';
+    };
+    
+    };
+}
--- src/oobe.py
+++ src/oobe.py
@@ -30,8 +30,40 @@ class ZenWelcomeWindow(Adw.ApplicationWindow):
         self.is_playing = False
         self.can_close = False
         self.timer_id = 0
+        self.transition_started = False
+
+        self.current_frame = 1
+        self.total_frames = 600
+        self.frame_timer = 0
+
+        self.frame_dir = os.environ.get("ZENOS_FRAMES_DIR", "/run/current-system/sw/share/zenos/frames")
+
+        self.settings = Gio.Settings.new('org.gnome.desktop.interface')
+        self.og_anim_state = self.settings.get_boolean('enable-animations')
+        self.bg_settings = Gio.Settings.new('org.gnome.desktop.background')
+
+        # set up dbus proxy for toggling cursed extensions
+        try:
+            bus = Gio.bus_get_sync(Gio.BusType.SESSION, None)
+            self.ext_proxy = Gio.DBusProxy.new_sync(
+                bus,
+                Gio.DBusProxyFlags.NONE,
+                None,
+                "org.gnome.Shell.Extensions",
+                "/org/gnome/Shell/Extensions",
+                "org.gnome.Shell.Extensions",
+                None
+            )
+        except Exception:
+            self.ext_proxy = None
+
+        # step 1: set wallpaper black, disable animations immediately
+        self.set_global_anims(False)
+        self.bg_settings.set_string('picture-options', 'none')
+        self.bg_settings.set_string('primary-color', '#000000')
 
         self.connect("close-request", self.on_close_request)
+        self.connect("map", self.on_window_mapped)
 
         self.overlay = Gtk.Overlay()
         self.set_content(self.overlay)
@@ -40,12 +72,6 @@ class ZenWelcomeWindow(Adw.ApplicationWindow):
         self.picture.set_hexpand(True)
         self.picture.set_vexpand(True)
         self.picture.set_content_fit(Gtk.ContentFit.COVER)
-
-        # Load from GResource
-        self.media_file = Gtk.MediaFile.new_for_resource("/com/negzero/zenos/setup/assets/welcome.mp4")
-        self.media_file.connect("notify::ended", self.on_media_finished)
-        self.picture.set_paintable(self.media_file)
-
         self.overlay.set_child(self.picture)
 
         self.skip_button = ZenAnimatedButton(label="Skip Intro")
@@ -55,7 +81,6 @@ class ZenWelcomeWindow(Adw.ApplicationWindow):
         self.skip_button.set_valign(Gtk.Align.END)
         self.skip_button.set_margin_bottom(60)
         self.skip_button.connect("clicked", self.on_skip_clicked)
-
         self.overlay.add_overlay(self.skip_button)
 
         target = Adw.PropertyAnimationTarget.new(self.skip_button, 'progress')
@@ -63,23 +88,70 @@ class ZenWelcomeWindow(Adw.ApplicationWindow):
         self.animation = Adw.SpringAnimation.new(self.skip_button, 0.0, 1.0, params, target)
 
         self.setup_input_tracking()
-
-        # Start playback immediately for the OOBE feel
-        self.media_file.set_loop(False)
-        self.media_file.play()
-        self.is_playing = True
         self.fullscreen()
-        
-    def on_media_finished(self, media_file, pspec):
-        if media_file.get_ended():
-            # reuse your skip logic to clean up and emit signal
-            self.on_skip_clicked(None)
+
+        self.start_playback()
+
+    def set_global_anims(self, state):
+        self.settings.set_boolean('enable-animations', state)
+        Gio.Settings.sync()
+
+        if self.ext_proxy:
+            method = "EnableExtension" if state else "DisableExtension"
+            try:
+                self.ext_proxy.call_sync(
+                    method,
+                    GLib.Variant('(s)', ('burn-my-windows@schneegans.github.com',)),
+                    Gio.DBusCallFlags.NONE,
+                    -1,
+                    None
+                )
+            except Exception:
+                pass
+
+    def on_window_mapped(self, *args):
+        # step 2: wait 50ms after the window physically maps to switch it up
+        GLib.timeout_add(50, self.step_two_enable_anims_and_bg)
+
+    def step_two_enable_anims_and_bg(self):
+        self.set_global_anims(self.og_anim_state)
+        final_frame = os.path.join(self.frame_dir, f"frame_{self.total_frames:04d}.png")
+        target_wallpaper = f"file://{final_frame}"
+
+        self.bg_settings.set_string('picture-options', 'zoom')
+        self.bg_settings.set_string('picture-uri', target_wallpaper)
+        self.bg_settings.set_string('picture-uri-dark', target_wallpaper)
+        return False
+
+    def start_playback(self):
+        self.is_playing = True
+        self.frame_timer = GLib.timeout_add(33, self.next_frame)
+
+    def next_frame(self):
+        if not self.is_playing or self.transition_started:
+            return False
+
+        # step 3: wait until the last 10 frames -> disable animations again
+        if self.current_frame == self.total_frames - 10:
+            self.set_global_anims(False)
+
+        if self.current_frame > self.total_frames:
+            self.trigger_transition()
+            return False
+
+        frame_name = f"frame_{self.current_frame:04d}.png"
+        frame_path = os.path.join(self.frame_dir, frame_name)
+
+        file = Gio.File.new_for_path(frame_path)
+        self.picture.set_file(file)
+
+        self.current_frame += 1
+        return True
 
     def setup_input_tracking(self):
         key_controller = Gtk.EventControllerKey()
         key_controller.connect("key-pressed", self.on_input_detected)
         self.add_controller(key_controller)
-
         click_controller = Gtk.GestureClick()
         click_controller.connect("pressed", self.on_input_detected)
         self.add_controller(click_controller)
@@ -89,7 +161,6 @@ class ZenWelcomeWindow(Adw.ApplicationWindow):
             self.animation.set_value_from(self.skip_button.progress)
             self.animation.set_value_to(1.0)
             self.animation.play()
-
             if self.timer_id > 0:
                 GLib.source_remove(self.timer_id)
             self.timer_id = GLib.timeout_add(2000, self.hide_skip_button)
@@ -106,8 +177,32 @@ class ZenWelcomeWindow(Adw.ApplicationWindow):
         return not self.can_close
 
     def on_skip_clicked(self, btn):
-        if self.media_file:
-            self.media_file.pause()
+        self.trigger_transition()
+
+    def trigger_transition(self):
+        if self.transition_started:
+            return False
+        self.transition_started = True
         self.is_playing = False
         self.can_close = True
+
+        # violently kill animations right here so the close anim is definitely dead
+        self.set_global_anims(False)
+
+        # kill the video window instantly while anims are dead
+        self.set_visible(False)
+
+        # wait 50ms, then re-enable anims
+        GLib.timeout_add(50, self._phase2_enable_anims)
+        return False
+
+    def _phase2_enable_anims(self):
+        self.set_global_anims(self.og_anim_state)
+
+        # wait 150ms for mutter to process the anim state change, then open setup
+        GLib.timeout_add(150, self._phase3_open_window)
+        return False
+
+    def _phase3_open_window(self):
         self.emit('intro-skipped')
+        return False
